90742f4005bc6f8d5e9d0dfa8536b18f3e595b81
dhdhagai <84077442+dhdhagai@users.noreply.github.com>
2022-02-13 20:45:48 +0530

    Initial Commit after readme.md

---
+++ script.js
@@ -0,0 +1,7 @@
+function ani() {
+  document.getElementById("one").className = "one";
+  document.getElementById("two").className = "two";
+  document.getElementById("three").className = "three";
+  document.getElementById("four").className = "four";
+  document.getElementById("buton").style.display = "none";
+}
